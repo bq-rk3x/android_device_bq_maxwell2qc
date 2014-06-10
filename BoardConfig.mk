f3ae15b21c3513e15630af09cf01de000beec93a
fefifofum <jorsellis@gmail.com>
2014-06-10 03:37:29 +0200

    Cleanup BoardConfig

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -16,21 +16,10 @@
 
 -include device/bq/rockchip-common/BoardConfigCommon.mk
 
--include vendor/bq/maxwell2qc/BoardConfigVendor.mk
-
 TARGET_BOOTLOADER_BOARD_NAME := bq_Maxwell2_QuadCore
 
 TARGET_OTA_ASSERT_DEVICE := maxwell2qc,bq_Maxwell2_QuadCore
 
-# Bluetooth
 BOARD_BLUETOOTH_BDROID_BUILDCFG_INCLUDE_DIR := device/bq/maxwell2qc/bluetooth
 
-# Partitions
-BOARD_BOOTIMAGE_PARTITION_SIZE := 16777216
-BOARD_RECOVERYIMAGE_PARTITION_SIZE := 16777216
-BOARD_SYSTEMIMAGE_PARTITION_SIZE := 576716800
-BOARD_USERDATAIMAGE_PARTITION_SIZE := 14906032128
-BOARD_FLASH_BLOCK_SIZE := 16384
-
-# Kernel
 TARGET_KERNEL_CONFIG := cyanogenmod_maxwell2qc_defconfig
